009f487e15810b7b0319e6fbd501823bdd8e93f6
Kevin <kfish610@gmail.com>
2020-01-08 21:39:49 -0800

    Normalize names, refactor source.

---
+++ Source/BiomesCore/BiomesCore/BiomesCore.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace BiomesCore
+{
+    public static class BiomesCore
+    {
+        public const string Id = "rimworld.biomes.core";
+        public const string Name = "Biomes! Core";
+        public static string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        static BiomesCore()
+        {
+            HarmonyInstance.Create(Id).PatchAll();
+            Log("Initialized");
+        }
+
+        public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
+
+        private static string PrefixMessage(string message) => $"[{Name} v{Version}] {message}";
+    }
+}
--- Source/BiomesCore/BiomesCore/Properties/AssemblyInfo.cs
+++ Source/BiomesCore/BiomesCore/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RWBIomes_Core")]
+[assembly: AssemblyTitle("BiomesCore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RWBIomes_Core")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("Biomes Core")]
+[assembly: AssemblyCopyright("Copyright © The Biomes Team 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
