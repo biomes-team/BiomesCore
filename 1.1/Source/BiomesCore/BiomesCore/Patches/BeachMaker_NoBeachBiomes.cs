1e11007ea02d3a70a6ac98e0648872b08917e35d
zyllle <zyll3on@gmail.com>
2020-03-06 11:34:32 -0700

    Boat compatable version of the beach-removal patch

---
+++ 1.1/Source/BiomesCore/BiomesCore/Patches/BeachMaker_NoBeachBiomes.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld.Planet;
+using RimWorld;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(BeachMaker), nameof(BeachMaker.Init))]
+    internal static class BeachMaker_NoBeachBiomes
+    {
+        // from RF-Archipelagos
+        internal static bool Prefix(Map map)
+        {
+            if (map.Biome.defName.Contains("NoBeach"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
