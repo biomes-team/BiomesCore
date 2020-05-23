a49ef47b8abd8f47751215d416eb1cec3cddc47f
Matthew Seal <mseal007@gmail.com>
2020-05-22 23:12:58 -0700

    Added water plants viability

---
+++ 1.1/Source/BiomesCore/BiomesCore/DefModExtensions/Plant_Water.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace BiomesCore.DefModExtensions
+{
+    public class Biomes_WaterPlant : DefModExtension
+    {
+        public bool allowInFreshWater = true;
+        public bool allowInSaltWater = false;
+        public bool allowInShallowWater = false;
+        public bool allowInDeepWater = false;
+        public bool allowOnLand = false;
+    }
+}
---
+++ 1.1/Source/BiomesCore/BiomesCore/Patches/WaterPlant.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection.Emit;
+using BiomesCore.Reflections;
+using BiomesCore.Bridges;
+using BiomesCore.DefModExtensions;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(PlantUtility), "CanEverPlantAt")]
+    internal static class PlantUtility_CanEverPlantAt
+    {
+        internal static bool Prefix(ref bool __result, ThingDef plantDef, IntVec3 c, Map map)
+        {
+            TerrainDef terrain = map.terrainGrid.TerrainAt(c);
+            if (terrain.HasTag("Water"))
+            {
+                if (!plantDef.HasModExtension<Biomes_WaterPlant>())
+                {
+                    __result = false;
+                    return false;
+                }
+                Biomes_WaterPlant ext = plantDef.GetModExtension<Biomes_WaterPlant>();
+                if ((terrain.HasTag("Salty") && !ext.allowInSaltWater) || (!terrain.HasTag("Salty") && !ext.allowInFreshWater) || (terrain.HasTag("Deep") && !ext.allowInDeepWater) || (!terrain.HasTag("Deep") && !ext.allowInShallowWater))
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+            else if (plantDef.HasModExtension<Biomes_WaterPlant>() )
+            {
+                Biomes_WaterPlant ext = plantDef.GetModExtension<Biomes_WaterPlant>();
+                if (!ext.allowOnLand)
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
