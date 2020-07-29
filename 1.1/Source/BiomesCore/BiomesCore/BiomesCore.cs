56b8fe5ba5ff5490a47898234d419e90c0d084fe
OdeumBiomes <59748847+OdeumBiomes@users.noreply.github.com>
2020-07-29 05:10:05 +0200

    Ready for kev.

--- 1.1/Source/BiomesCore/BiomesCore/BiomesCore.cs
+++ 1.1/Source/BiomesCore/BiomesCore/BiomesCore.cs
@@ -5,19 +5,21 @@ using Verse;
 
 namespace BiomesCore
 {
+    public sealed class BiomesCoreMod : Mod
+    {
+        public BiomesCoreMod(ModContentPack content) : base(content)
+        {
+            new Harmony(BiomesCore.Id).PatchAll();
+            BiomesCore.Log("Initialized");
+        }
+    }
+
     [StaticConstructorOnStartup]
     public static class BiomesCore
     {
         public const string Id = "rimworld.biomes.core";
         public const string Name = "Biomes! Core";
         public static string Version = (Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute).InformationalVersion;
-
-        static BiomesCore()
-        {
-            //HarmonyInstance.Create(Id).PatchAll();
-            new Harmony(Id).PatchAll();
-            Log("Initialized");
-        }
 
         public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
 
