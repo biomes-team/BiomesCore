754cfd4cc61dc8ba3ff33e27e4cf8f01a39c07c4
zyllle <zyll3on@gmail.com>
2020-08-29 22:49:46 -0600

    Growing zones can only be placed on land

---
+++ Source/BiomesCore/BiomesCore/Patches/LandGrowingZones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BiomesCore.Patches
+{
+    /// <summary>
+    /// Prevent normal growing zones from being placed in water
+    /// </summary>
+    [HarmonyPatch(typeof(RimWorld.Designator_ZoneAdd_Growing), "CanDesignateCell")]
+    internal static class LandGrowingZones
+    {
+        static bool Prefix(IntVec3 c, ref AcceptanceReport __result)
+        {
+            if (Find.CurrentMap.terrainGrid.TerrainAt(c).IsWater)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
