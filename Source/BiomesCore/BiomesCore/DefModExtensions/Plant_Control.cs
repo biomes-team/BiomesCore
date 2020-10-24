9081e554ac5e3d03193b039f2ce4334dec71a488
OdeumBiomes <59748847+OdeumBiomes@users.noreply.github.com>
2020-10-25 01:45:16 +0200

    Plant density overhaul

---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/Plant_Control.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace BiomesCore.DefModExtensions
+{
+    public class Biomes_WaterPlant : DefModExtension
+    {
+        public bool allowInFreshWater = false;
+        public bool allowInSaltWater = false;
+        public bool allowInShallowWater = false;
+        public bool allowInDeepWater = false;
+        public bool allowOnLand = false;
+    }
+    public class Biomes_SandPlant  : DefModExtension
+    {
+        public bool allowOnSand = false;
+        public bool allowOffSand = false;
+    }
+
+    public class Biomes_BiomePlantControl : DefModExtension
+    {
+        public float biomeTotalCommonality = 0f;
+        public float biomeWaterCommonality = 0f;
+        public float biomeSandCommonality = 0f;
+        public float biomeLandCommonality = 0f;
+    }
+}
--- Source/BiomesCore/BiomesCore/Patches/NewWaterPlant.cs
+++ Source/BiomesCore/BiomesCore/Patches/NewWaterPlant.cs
@@ -7,45 +7,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BiomesCore.DefModExtensions;
-
+[StaticConstructorOnStartup]
+static class WildSpawnPreIterator
+{
+}
 namespace BiomesCore.Patches
 {
-    [HarmonyPatch(typeof(WildPlantSpawner), "GetCommonalityOfPlant")]
-    internal static class WildPlantSpawner_GetCommonalityOfPlant
+
+    [StaticConstructorOnStartup]
+    static class CommonalityFinder
     {
-        internal static bool Prefix(ref float __result, ThingDef plant, Map ___map)
+        public static float landCommonality = 0;
+        public static float waterCommonality = 0;
+        public static float sandCommonality = 0;
+        public static Dictionary<string, float> commonalitySum = new Dictionary<string, float>();
+
+        static CommonalityFinder()
         {
-            float totalPlants = 0;
-            float landPlants = 0;
-            float waterPlants = 0;
-            float sandPlants = 0;
-            foreach (ThingDef plantdef in ___map.Biome.AllWildPlants)
+            foreach (BiomeDef biome in DefDatabase<BiomeDef>.AllDefs.ToList())
             {
-                totalPlants += ___map.Biome.CommonalityOfPlant(plant); ;
-                if (plant.HasModExtension<Biomes_WaterPlant>())
+                foreach (ThingDef plant in biome.AllWildPlants)
                 {
-                    waterPlants += ___map.Biome.CommonalityOfPlant(plant);
+                    if (plant.HasModExtension<Biomes_WaterPlant>())
+                    {
+                        waterCommonality += biome.CommonalityOfPlant(plant);
+                        continue;
+                    }
+                    if (plant.HasModExtension<Biomes_SandPlant>())
+                    {
+                        sandCommonality += biome.CommonalityOfPlant(plant);
+                        continue;
+                    }
+                    landCommonality += biome.CommonalityOfPlant(plant);
                     continue;
+
                 }
-                if (plant.HasModExtension<Biomes_SandPlant>())
-                {
-                    sandPlants += ___map.Biome.CommonalityOfPlant(plant); ;
-                    continue;
-                }
-                landPlants += ___map.Biome.CommonalityOfPlant(plant); ;
+                sandCommonality /= landCommonality;
+                commonalitySum.Add(biome + "_SandCommonality", sandCommonality);
+                waterCommonality /= landCommonality;
+                commonalitySum.Add(biome + "_WaterCommonality", waterCommonality);
+                landCommonality /= landCommonality;
+                commonalitySum.Add(biome + "_LandCommonality", landCommonality);
             }
-            if (plant.HasModExtension<Biomes_WaterPlant>())
-            {
-                __result = (___map.Biome.CommonalityOfPlant(plant) * waterPlants) / (totalPlants / 3);
-                return false;
-            }
-            if (plant.HasModExtension<Biomes_SandPlant>())
-            {
-                __result = (___map.Biome.CommonalityOfPlant(plant) * sandPlants) / (totalPlants / 3);
-                return false;
-            }
-            __result = (___map.Biome.CommonalityOfPlant(plant) * landPlants) / (totalPlants / 3);
-            return true;
         }
     }
+
+    [HarmonyPatch(typeof(WildPlantSpawner), "GetBaseDesiredPlantsCountAt")]
+    internal static class WildPlantSpawner_GetBaseDesiredPlantsCountAt
+    {
+        internal static void Postfix(ref float __result, IntVec3 c, Map ___map)
+        {
+            Dictionary<string, float> commonalitySum = CommonalityFinder.commonalitySum;
+            var terrain = c.GetTerrain(___map);
+            var biome = ___map.Biome;
+            float fertility = terrain.fertility;
+            if (terrain.HasTag("Water"))
+            {
+                __result = fertility * commonalitySum[biome + "_WaterCommonality"];
+                return;
+            }
+            if (terrain.HasTag("Sandy"))
+            {
+                __result = fertility * commonalitySum[biome + "_SandCommonality"];
+                return;
+            }
+            __result = fertility * commonalitySum[biome + "_LandCommonality"];
+            return;
+
+        }
+    }
+
+
+    //[HarmonyPatch(typeof(WildPlantSpawner), "GetCommonalityOfPlant")]
+    //internal static class WildPlantSpawner_GetCommonalityOfPlant
+    //{
+    //    internal static bool Prefix(ref float __result, ThingDef plant, Map ___map)
+    //    {
+    //        float totalPlants = 0;
+    //        float landPlants = 0;
+    //        float waterPlants = 0;
+    //        float sandPlants = 0;
+    //        foreach (ThingDef plantdef in ___map.Biome.AllWildPlants)
+    //        {
+    //            totalPlants += ___map.Biome.CommonalityOfPlant(plant); ;
+    //            if (plant.HasModExtension<Biomes_WaterPlant>())
+    //            {
+    //                waterPlants += ___map.Biome.CommonalityOfPlant(plant);
+    //                continue;
+    //            }
+    //            if (plant.HasModExtension<Biomes_SandPlant>())
+    //            {
+    //                sandPlants += ___map.Biome.CommonalityOfPlant(plant); ;
+    //                continue;
+    //            }
+    //            Log.Message("This is a land plant " + plant.defName);
+    //            landPlants += ___map.Biome.CommonalityOfPlant(plant); ;
+    //        }
+    //        if (plant.HasModExtension<Biomes_WaterPlant>())
+    //        {
+    //            float test = (___map.Biome.CommonalityOfPlant(plant) * waterPlants) / (totalPlants / 3);
+    //            Log.Message("Setting commonality of " + plant + " to " + test);
+    //            __result = (___map.Biome.CommonalityOfPlant(plant) * waterPlants) / (totalPlants / 3);
+    //            return false;
+    //        }
+    //        if (plant.HasModExtension<Biomes_SandPlant>())
+    //        {
+    //            __result = (___map.Biome.CommonalityOfPlant(plant) * sandPlants) / (totalPlants / 3);
+    //            return false;
+    //        }
+    //        __result = (___map.Biome.CommonalityOfPlant(plant) * landPlants) / (totalPlants / 3);
+    //        return true;
+    //    }
+    //}
 }
