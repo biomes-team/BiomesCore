77663b1a4e2d931e4de988f268b8781113d37142
CosmicOdeum <59748847+OdeumBiomes@users.noreply.github.com>
2021-03-04 05:54:27 +0100

    custom plant class for testing.

---
+++ Source/BiomesCore/BiomesCore/Derived Classes/PlantClasses.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesCore.Derived_Classes
+{
+    class TestPlant : Plant
+    {
+        protected override bool Resting => true;
+
+    }
+}
