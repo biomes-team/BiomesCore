d31d1e23896c84771283208ffbff2d95e5ef07cd
CosmicOdeum <59748847+OdeumBiomes@users.noreply.github.com>
2021-07-07 00:21:33 +0200

    archipelago releasable

--- Source/BiomesCore/BiomesCore/MapGeneration/GenStep_IslandShape_Cluster.cs
+++ Source/BiomesCore/BiomesCore/MapGeneration/GenStep_IslandShape_Cluster.cs
@@ -36,11 +36,15 @@ namespace BiomesCore.MapGeneration
             float angleB = angleA + Rand.Range(60f, 120f);
             float angleC = angleB + Rand.Range(60f, 120f);
             float angleD = angleC + Rand.Range(60f, 120);
+            float angleE = angleD + Rand.Range(60f, 120);
+            float angleF = angleE + Rand.Range(60f, 120);
 
             IntVec3 centerA = mapCenter + (distRange.RandomInRange * Vector3Utility.FromAngleFlat(angleA)).ToIntVec3();
             IntVec3 centerB = mapCenter + (distRange.RandomInRange * 0.5f * Vector3Utility.FromAngleFlat(angleB)).ToIntVec3();
             IntVec3 centerC = mapCenter + (distRange.RandomInRange * 1.3f * Vector3Utility.FromAngleFlat(angleC)).ToIntVec3();
             IntVec3 centerD = mapCenter + (distRange.RandomInRange * Vector3Utility.FromAngleFlat(angleD)).ToIntVec3();
+            IntVec3 centerE = mapCenter + (distRange.RandomInRange * 0.5f * Vector3Utility.FromAngleFlat(angleE)).ToIntVec3();
+            IntVec3 centerF = mapCenter + (distRange.RandomInRange * 1.3f * Vector3Utility.FromAngleFlat(angleF)).ToIntVec3();
 
             ModuleBase noiseA = new Perlin(Rand.Range(0.015f, 0.028f), 2.0, 0.5, 6, Rand.Range(0, 2147483647), QualityMode.High);
             ModuleBase noiseB = new Perlin(Rand.Range(0.015f, 0.028f), 2.0, 0.5, 6, Rand.Range(0, 2147483647), QualityMode.High);
@@ -60,6 +64,8 @@ namespace BiomesCore.MapGeneration
                 float distB = (float)Math.Sqrt(Math.Pow(current.x - centerB.x, 2) + Math.Pow(current.z - centerB.z, 2));
                 float distC = (float)Math.Sqrt(Math.Pow(current.x - centerC.x, 2) + Math.Pow(current.z - centerC.z, 2));
                 float distD = (float)Math.Sqrt(Math.Pow(current.x - centerD.x, 2) + Math.Pow(current.z - centerD.z, 2));
+                float distE = (float)Math.Sqrt(Math.Pow(current.x - centerE.x, 2) + Math.Pow(current.z - centerE.z, 2));
+                float distF = (float)Math.Sqrt(Math.Pow(current.x - centerF.x, 2) + Math.Pow(current.z - centerF.z, 2));
 
                 // island A
                 float addition = Math.Max(0, 20 * (1f - (sizeA * distA / mapSize)) + noisinessA * noiseA.GetValue(current));
@@ -72,7 +78,15 @@ namespace BiomesCore.MapGeneration
 
                 // island D
                 addition += Math.Max(0, 20 * (1f - (sizeB * distD / mapSize)) + noisinessB * noiseB.GetValue(current));
-
+                switch (islandNumber)
+                {
+                    case int temp when temp >= 5:
+                        addition += Math.Max(0, 20 * (1f - (sizeA * distE / mapSize)) + noisinessB * noiseB.GetValue(current));
+                        break;
+                    case 5:
+                        addition += Math.Max(0, 20 * (1f - (sizeB * distF / mapSize)) + noisinessB * noiseB.GetValue(current));
+                        break;
+                }
                 fertility[current] += addition;
             }
         }
