687dcb11465d8320973e723edd659e5267e66b56
CosmicOdeum <59748847+OdeumBiomes@users.noreply.github.com>
2021-07-16 12:01:36 +0200

    cave plants die to light

--- Source/BiomesCore/BiomesCore/DerivedClasses/PlantClasses.cs
+++ Source/BiomesCore/BiomesCore/DerivedClasses/PlantClasses.cs
@@ -9,29 +9,44 @@ using Verse;
 
 namespace BiomesCore.DerivedClasses
 {
-    public class BMT_Plant : Plant
-    {
-        protected override bool Resting => isResting;
-        bool isResting
-        {
-            get
-            {
-                if (def.HasModExtension<Biomes_PlantControl>())
-                {
-                    Biomes_PlantControl ext = def.GetModExtension<Biomes_PlantControl>();
-                    if (ext.needsRest)
-                    {
-                        if (!(GenLocalDate.DayPercent(this) < ext.growingHours.min))
-                        {
-                            return GenLocalDate.DayPercent(this) > ext.growingHours.max;
-                        }
-                        return true;
-                    }
-                    return false;
-                }
-                // returns the default Resting value without having to copy code
-                return base.Resting;
-            }
-        }
-    }
+	public class BMT_Plant : Plant
+	{
+		protected override bool Resting => isResting;
+		bool isResting
+		{
+			get
+			{
+				if (def.HasModExtension<Biomes_PlantControl>())
+				{
+					Biomes_PlantControl ext = def.GetModExtension<Biomes_PlantControl>();
+					if (ext.needsRest)
+					{
+						if (!(GenLocalDate.DayPercent(this) < ext.growingHours.min))
+						{
+							return GenLocalDate.DayPercent(this) > ext.growingHours.max;
+						}
+						return true;
+					}
+					return false;
+				}
+				// returns the default Resting value without having to copy code
+				return base.Resting;
+			}
+		}
+		public override bool DyingBecauseExposedToLight
+		{
+			get
+			{
+				if (def.HasModExtension<Biomes_PlantControl>())
+				{
+					Biomes_PlantControl ext = def.GetModExtension<Biomes_PlantControl>();
+					if (ext.cavePlant)
+					{
+						return base.Map.glowGrid.GameGlowAt(base.Position, ignoreCavePlants: true) > 0f;
+					}
+				}
+				return base.DyingBecauseExposedToLight;
+			}
+		}
+	}
 }
