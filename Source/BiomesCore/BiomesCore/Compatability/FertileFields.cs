9ad43108e8081a9472576884266252de7adb35db
Matthew Seal <mseal007@gmail.com>
2021-09-06 10:09:03 -0700

    Adde support for pebbles to be terraformed by fertile fields

---
+++ Source/BiomesCore/BiomesCore/Compatability/FertileFields.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace BiomesCore.Compatability
+{
+    [StaticConstructorOnStartup]
+    public static class FertileFieldsCompatibilityPatch
+    {
+        static FertileFieldsCompatibilityPatch()
+        {
+            TerrainAffordanceDef fertileGravelAffordance = DefDatabase<TerrainAffordanceDef>.GetNamed("Gravel", false);
+            if (fertileGravelAffordance != null)
+            {
+                TerrainDef pebbles = TerrainDef.Named("BiomesCore_Pebbles");
+                pebbles.affordances.Add(fertileGravelAffordance);
+            }
+        }
+    }
+}
