037fb4951906f65ee2f81d71ee16aa8d082130bc
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-09-08 18:49:36 -0600

    tunnel map adjustment

--- Source/BiomesCore/BiomesCore/MapGeneration/GenStep_CavernShape_NarrowTunnels.cs
+++ Source/BiomesCore/BiomesCore/MapGeneration/GenStep_CavernShape_NarrowTunnels.cs
@@ -26,12 +26,32 @@ namespace BiomesCore.MapGeneration
 
 			ModuleBase noise = new Perlin(0.02, 1.5, 0.5, 6, Rand.Range(0, int.MaxValue), QualityMode.Medium);
 
+			
+
 			MapGenFloatGrid elevation = MapGenerator.Elevation;
 
 			foreach (IntVec3 cell in map.AllCells)
 			{
                 elevation[cell] = 0.53f + 2f * Math.Abs(tunnels.GetValue(cell) - 0.5f) + 0.10f * noise.GetValue(cell);
 			}
+
+
+			IntVec3 roomLoc = map.Center;
+			roomLoc.x = (int)(Rand.Range(0.3f, 1.7f) * roomLoc.x);
+			roomLoc.z = (int)(Rand.Range(0.3f, 1.7f) * roomLoc.z);
+
+			ModuleBase roomNoise = new Perlin(Rand.Range(0.015f, 0.028f), 2.0, 0.5, 6, Rand.Range(0, 2147483647), QualityMode.High);
+			float sizeAdj = Rand.Range(8f, 10f);
+
+			foreach (IntVec3 cell in map.AllCells)
+			{
+				float distance = (float)Math.Sqrt(Math.Pow(cell.x - roomLoc.x, 2) + Math.Pow(cell.z - roomLoc.z, 2));
+
+				float addition = 25 * (1f - (sizeAdj * distance / map.Size.x)) + 15f * roomNoise.GetValue(cell);
+
+				elevation[cell] -= Math.Max(0, addition);
+			}
+
 
 		}
 	}
