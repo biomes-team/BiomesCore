88ed17526c5fe5effd950315dc0be8278e231ae6
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-09-28 15:48:19 -0600

    Event patches

---
+++ Source/BiomesCore/BiomesCore/Patches/CavernEvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+using HarmonyLib;
+using BiomesCore.DefModExtensions;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(IncidentWorker_Aurora), "CanFireNowSub")]
+    static class CavernAuroraPatch
+    {
+        static bool Prefix(IncidentParms parms, ref bool __result)
+        {
+            List<Map> maps = Find.Maps;
+            for (int i = 0; i < maps.Count; i++)
+            {
+                if (maps[i].IsPlayerHome && !maps[i].Biome.HasModExtension<BiomesMap>())
+                {
+                    return true;
+                }
+                if (!maps[i].Biome.GetModExtension<BiomesMap>().isCavern)
+                {
+                    return true;
+                }
+            }
+
+            __result = false;
+            return false;
+        }
+    }
+
+
+
+}
