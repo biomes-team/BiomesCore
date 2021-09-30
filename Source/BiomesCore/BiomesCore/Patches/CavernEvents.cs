e4d2506da0e8e88686c2bc60db6fa347b1a54b8d
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-09-29 18:38:58 -0600

    eclipse, fallout, and flashstorm patches for Caverns

--- Source/BiomesCore/BiomesCore/Patches/CavernEvents.cs
+++ Source/BiomesCore/BiomesCore/Patches/CavernEvents.cs
@@ -35,5 +35,74 @@ namespace BiomesCore.Patches
     }
 
 
+    [HarmonyPatch(typeof(IncidentWorker_MakeGameCondition), "CanFireNowSub")]
+    static class CavernEclipseAndFalloutPatch
+    {
+        static bool Prefix(IncidentParms parms, ref IncidentWorker __instance, ref bool __result)
+        {
 
+            if (__instance.def == IncidentDefOf.ToxicFallout)
+            {
+                BiomeDef biome = Find.WorldGrid[parms.target.Tile].biome;
+                if (!biome.HasModExtension<BiomesMap>())
+                {
+                    return true;
+                }
+                if (!biome.GetModExtension<BiomesMap>().isCavern)
+                {
+                    return true;
+                }
+                __result = false;
+                return false;
+            }
+
+
+            if (__instance.def == IncidentDefOf.Eclipse)
+            {
+                List<Map> maps = Find.Maps;
+                for (int i = 0; i < maps.Count; i++)
+                {
+                    if (maps[i].IsPlayerHome && !maps[i].Biome.HasModExtension<BiomesMap>())
+                    {
+                        return true;
+                    }
+                    if (!maps[i].Biome.GetModExtension<BiomesMap>().isCavern)
+                    {
+                        return true;
+                    }
+                }
+
+                __result = false;
+                return false;
+            }
+
+
+            return true;
+
+        }
+    }
+
+    [HarmonyPatch(typeof(IncidentWorker_Flashstorm), "CanFireNowSub")]
+    static class CavernFlashstormPatch
+    {
+        static bool Prefix(IncidentParms parms, ref IncidentWorker __instance, ref bool __result)
+        {
+            if (__instance.def == IncidentDefOf.Eclipse || __instance.def == IncidentDefOf.ToxicFallout)
+            {
+                BiomeDef biome = Find.WorldGrid[parms.target.Tile].biome;
+                if (!biome.HasModExtension<BiomesMap>())
+                {
+                    return true;
+                }
+                if (!biome.GetModExtension<BiomesMap>().isCavern)
+                {
+                    return true;
+                }
+                __result = false;
+                return false;
+            }
+            return true;
+
+        }
+    }
 }
