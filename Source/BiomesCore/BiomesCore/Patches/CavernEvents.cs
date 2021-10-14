4f844de6547ad9089400c77e44814d787ebcaa2f
Zylleon <56230821+Zylleon@users.noreply.github.com>
2021-10-13 19:25:44 -0600

    anima trees can spawn in caverns

--- Source/BiomesCore/BiomesCore/Patches/CavernEvents.cs
+++ Source/BiomesCore/BiomesCore/Patches/CavernEvents.cs
@@ -8,6 +8,7 @@ using Verse;
 using RimWorld;
 using HarmonyLib;
 using BiomesCore.DefModExtensions;
+using Verse.AI;
 
 namespace BiomesCore.Patches
 {
@@ -105,4 +106,93 @@ namespace BiomesCore.Patches
 
         }
     }
+
+
+    [HarmonyPatch(typeof(GenStep_AnimaTrees), "CanSpawnAt")]
+    static class CavernAnimaTreePatch
+    {
+        static bool Prefix(IntVec3 c, Map map, int minProximityToArtificialStructures, int minProximityToCenter, int minFertileUnroofedCells, int maxFertileUnroofedCellRadius, ref bool __result)
+        {
+            if (!map.Biome.HasModExtension<BiomesMap>())
+            {
+                return true;
+            }
+            if (!map.Biome.GetModExtension<BiomesMap>().isCavern)
+            {
+                return true;
+            }
+
+            // copied from the vanilla method, but with the "isroofed" checks replaced with UsesOutdoorTemperature
+
+            if (!c.Standable(map) || c.Fogged(map) || !c.GetRoom(map).UsesOutdoorTemperature)
+                {
+                __result = false;
+                return false;
+            }
+
+            Plant plant = c.GetPlant(map);
+            if (plant != null && plant.def.plant.growDays > 10f)
+            {
+                __result = false;
+                return false;
+            }
+
+            List<Thing> thingList = c.GetThingList(map);
+            for (int i = 0; i < thingList.Count; i++)
+            {
+                if (thingList[i].def == ThingDefOf.Plant_TreeAnima)
+                {
+                    __result = false;
+                    return false;
+                }
+            }
+            if (minProximityToCenter > 0 && map.Center.InHorDistOf(c, minProximityToCenter))
+            {
+                __result = false;
+                return false;
+            }
+            if (!map.reachability.CanReachFactionBase(c, map.ParentFaction))
+            {
+                __result = false;
+                return false;
+            }
+
+            TerrainDef terrain = c.GetTerrain(map);
+            if (terrain.avoidWander || terrain.fertility <= 0f)
+            {
+                __result = false;
+                return false;
+            }
+
+            if (minProximityToArtificialStructures != 0 && GenRadial.RadialDistinctThingsAround(c, map, minProximityToArtificialStructures, useCenter: false).Any(MeditationUtility.CountsAsArtificialBuilding))
+            {
+                __result = false;
+                return false;
+            }
+            int num = GenRadial.NumCellsInRadius(maxFertileUnroofedCellRadius);
+            int num2 = 0;
+
+            for (int j = 0; j < num; j++)
+            {
+                IntVec3 intVec = c + GenRadial.RadialPattern[j];
+                if (WanderUtility.InSameRoom(intVec, c, map))
+                {
+                    if (intVec.InBounds(map) && intVec.UsesOutdoorTemperature(map) && intVec.GetTerrain(map).fertility > 0f)
+                    {
+                        num2++;
+                    }
+                    if (num2 >= minFertileUnroofedCells)
+                    {
+                        __result = true;
+                        return false;
+                    }
+                }
+            }
+
+            __result = false;
+            return false;
+
+        }
+    }
+
 }
