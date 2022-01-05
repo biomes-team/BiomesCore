6c8286f647265191a841588c2e39f4d74c143d10
Evelyn <udderlyevelyn@gmail.com>
2022-01-05 13:34:46 -0500

    Fixed misnamed thing.

--- Source/BiomesCore/BiomesCore/Patches/DangerWatcher_CalculateDangerRating.cs
+++ Source/BiomesCore/BiomesCore/Patches/DangerWatcher_CalculateDangerRating.cs
@@ -14,14 +14,14 @@ namespace BiomesCore.Patches
         /// Holds the last value for danger level on a per-map basis 
         /// </summary>
         public static Dictionary<int, float> DangerRatingPerMap = new Dictionary<int, float>();
-        internal static void _putDangerRatinglPerMap(float value, int key)
+        internal static void _putDangerRatingPerMap(float value, int key)
         {
             //Log.Message("[BiomesCore] Assigning danger rating of " + value + " for map with ID " + key + " to the dictionary..");
             DangerRatingPerMap[key] = value;
         }
 
         [HarmonyDebug]
-        internal static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             bool match = false;
             var insts = instructions.ToList(); //So we can index into it.
@@ -35,7 +35,7 @@ namespace BiomesCore.Patches
                     yield return new CodeInstruction(OpCodes.Ldarg_0); //Get argument 0 to the method we're in, which is the instance ("this") and put it on the stack.
                     yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(DangerWatcher), "map")); //Get map for that instance (this is now on the stack instead).
                     yield return new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Map), "uniqueID")); //Get uniqueID for that map (this is now on the stack instead).
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DangerWatcher_CalculateDangerRating), "_putDangerLevelPerMap")); //Call our method using the stuff on the stack, leaving it how it was before we did anything.
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(DangerWatcher_CalculateDangerRating), "_putDangerRatingPerMap")); //Call our method using the stuff on the stack, leaving it how it was before we did anything.
                 }
             }
         }
