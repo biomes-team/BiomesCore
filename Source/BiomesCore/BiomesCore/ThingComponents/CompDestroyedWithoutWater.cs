6fb10a52487dd90bf851deebc4343352dadfb535
Evelyn <udderlyevelyn@gmail.com>
2022-02-14 22:25:31 -0500

    Added CompDestroyedWithoutWater, acts like CompTemperatureRuinable but insta-ruins the Thing if it's not on water (so you can cleanly swap it out for CompTemperatureRuinable with the same property settings).

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompDestroyedWithoutWater.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompDestroyedWithoutWater : CompTemperatureRuinable
+    {
+		private IntVec3 positionOnLastCheck = IntVec3.Invalid;
+		bool ruinedByLackOfWater = false;
+
+		private void DoTicks(int ticks)
+		{
+			//Copied this over from CompTemperatureRuinable since it's private..
+			if (!Ruined)
+			{
+				float ambientTemperature = parent.AmbientTemperature;
+				if (ambientTemperature > Props.maxSafeTemperature)
+				{
+					ruinedPercent += (ambientTemperature - Props.maxSafeTemperature) * Props.progressPerDegreePerTick * (float)ticks;
+				}
+				else if (ambientTemperature < Props.minSafeTemperature)
+				{
+					ruinedPercent -= (ambientTemperature - Props.minSafeTemperature) * Props.progressPerDegreePerTick * (float)ticks;
+				}
+				var pos = parent.Position;
+				if (positionOnLastCheck != pos)
+                {
+					positionOnLastCheck = pos;
+					if (!pos.GetTerrain(parent.MapHeld).IsWater)
+					{
+						ruinedPercent = 1f; //Ruined!
+						ruinedByLackOfWater = true;
+					}
+				}
+				if (ruinedPercent >= 1f)
+				{
+					ruinedPercent = 1f;
+					parent.BroadcastCompSignal("RuinedByTemperature");
+				}
+				else if (ruinedPercent < 0f)
+				{
+					ruinedPercent = 0f;
+				}
+			}
+		}
+
+        public override string CompInspectStringExtra()
+        {
+			if (ruinedByLackOfWater)
+				return "RuinedByLackOfWater".Translate();
+            return base.CompInspectStringExtra();
+        }
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompProperties_DestroyedWithoutWater.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompProperties_DestroyedWithoutWater : CompProperties_TemperatureRuinable
+    {
+
+    }
+}
