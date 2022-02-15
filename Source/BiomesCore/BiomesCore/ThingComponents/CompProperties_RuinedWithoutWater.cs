efd3bf0ed35624d011e79a6bc6e99c1776e9aaa0
Evelyn <udderlyevelyn@gmail.com>
2022-02-15 09:16:23 -0500

    Fixed mistake where new CompProps didn't have the new comp assigned.

--- Source/BiomesCore/BiomesCore/ThingComponents/CompProperties_RuinedWithoutWater.cs
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompProperties_RuinedWithoutWater.cs
@@ -10,6 +10,9 @@ namespace BiomesCore.ThingComponents
 {
     public class CompProperties_RuinedWithoutWater : CompProperties_TemperatureRuinable
     {
-
+        public CompProperties_RuinedWithoutWater()
+        {
+            compClass = typeof(CompRuinedWithoutWater);
+        }
     }
 }
