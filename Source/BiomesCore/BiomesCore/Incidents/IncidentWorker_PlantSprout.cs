7e70e7cbb68290edb5cc026556df4b82cf898e21
Evelyn <udderlyevelyn@gmail.com>
2022-02-24 22:15:14 -0500

    Made PlantSproutIncidentDef and changed the Ambrosyx Sprout to use this for configurability, changed the name of the Incident Worker to IncidentWorker_PlantSprout.

--- Source/BiomesCore/BiomesCore/Incidents/IncidentWorker_PlantSprout.cs
+++ Source/BiomesCore/BiomesCore/Incidents/IncidentWorker_PlantSprout.cs
@@ -6,28 +6,29 @@ using System.Threading.Tasks;
 using RimWorld;
 using Verse;
 
-namespace BiomesCore.IncidentWorkers
+namespace BiomesCore
 {
-	public class IncidentWorker_AmbrosyxSprout : IncidentWorker
+	public class IncidentWorker_PlantSprout : IncidentWorker
 	{
-		private static readonly IntRange CountRange = new IntRange(10, 20);
-
 		private const int MinRoomCells = 64;
 
 		private const int SpawnRadius = 6;
 
+		private PlantSproutIncidentDef incidentDef;
+
+		public IncidentWorker_PlantSprout()
+        {
+			incidentDef = (PlantSproutIncidentDef)def;
+		}
+
 		protected override bool CanFireNowSub(IncidentParms parms)
 		{
 			if (!base.CanFireNowSub(parms))
-			{
 				return false;
-			}
 			Map map = (Map)parms.target;
-			//if (!map.weatherManager.growthSeasonMemory.GrowthSeasonOutdoorsNow)
-			//{
-			//	return false;
-			//}
-			IntVec3 cell;
+            if (!incidentDef.ignoreSeason && !map.weatherManager.growthSeasonMemory.GrowthSeasonOutdoorsNow)
+                return false;
+            IntVec3 cell;
 			return TryFindRootCell(map, out cell);
 		}
 
@@ -35,28 +36,20 @@ namespace BiomesCore.IncidentWorkers
 		{
 			Map map = (Map)parms.target;
 			if (!TryFindRootCell(map, out var cell))
-			{
 				return false;
-			}
 			Thing thing = null;
-			int randomInRange = CountRange.RandomInRange;
+			int randomInRange = incidentDef.amount.RandomInRange;
 			for (int i = 0; i < randomInRange; i++)
 			{
 				if (!CellFinder.TryRandomClosewalkCellNear(cell, map, 6, out var result, (IntVec3 x) => CanSpawnAt(x, map)))
-				{
 					break;
-				}
 				result.GetPlant(map)?.Destroy();
-				Thing thing2 = GenSpawn.Spawn(PlantDefOf.BMT_Ambrosyx, result, map);
+				Thing thing2 = GenSpawn.Spawn(incidentDef.plant, result, map);
 				if (thing == null)
-				{
 					thing = thing2;
-				}
 			}
 			if (thing == null)
-			{
 				return false;
-			}
 			SendStandardLetter(parms, thing);
 			return true;
 		}
@@ -68,23 +61,15 @@ namespace BiomesCore.IncidentWorkers
 
 		private bool CanSpawnAt(IntVec3 c, Map map)
 		{
-			if (!c.Standable(map) || c.Fogged(map) || map.fertilityGrid.FertilityAt(c) < PlantDefOf.BMT_Ambrosyx.plant.fertilityMin || !c.GetRoom(map).PsychologicallyOutdoors || c.GetEdifice(map) != null || !PlantUtility.GrowthSeasonNow(c, map))
-			{
+			if (!c.Standable(map) || c.Fogged(map) || map.fertilityGrid.FertilityAt(c) < incidentDef.plant.plant.fertilityMin || !c.GetRoom(map).PsychologicallyOutdoors || c.GetEdifice(map) != null || (incidentDef.ignoreSeason && !PlantUtility.GrowthSeasonNow(c, map)))
 				return false;
-			}
 			Plant plant = c.GetPlant(map);
 			if (plant != null && plant.def.plant.growDays > 10f)
-			{
 				return false;
-			}
 			List<Thing> thingList = c.GetThingList(map);
 			for (int i = 0; i < thingList.Count; i++)
-			{
-				if (thingList[i].def == PlantDefOf.BMT_Ambrosyx)
-				{
+				if (thingList[i].def == incidentDef.plant)
 					return false;
-				}
-			}
 			return true;
 		}
 	}
---
+++ Source/BiomesCore/BiomesCore/Incidents/PlantSproutIncidentDef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace BiomesCore
+{
+    public class PlantSproutIncidentDef : IncidentDef
+    {
+        public ThingDef plant;
+        public IntRange amount = new IntRange(10, 20);
+        public bool ignoreSeason;
+    }
+}
