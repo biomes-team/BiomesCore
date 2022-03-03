e755fd3ec40972509a949827c58fc7091975b9b7
Evelyn <udderlyevelyn@gmail.com>
2022-03-03 18:40:47 -0500

    Changed the way Plant_GraphicPerBiome works to take paths and then load them and cache the graphic upon being called for.

--- Source/BiomesCore/BiomesCore/DefModExtensions/Plant_GraphicPerBiome.cs
+++ Source/BiomesCore/BiomesCore/DefModExtensions/Plant_GraphicPerBiome.cs
@@ -15,44 +15,64 @@ namespace BiomesCore.DefModExtensions
         public List<Graphic> sowingGraphics;
         public List<Graphic> leaflessGraphics;
         public List<Graphic> immatureGraphics;
-        
+        public List<string> graphicPaths;
+        public List<string> sowingGraphicPaths;
+        public List<string> leaflessGraphicPaths;
+        public List<string> immatureGraphicPaths;
+
         public Graphic GraphicForBiome(BiomeDef biome)
         {
-            if (graphics == null)
+            if (graphicPaths == null) 
                 return null;
+            if (graphics == null)
+                graphics = new List<Graphic>(graphicPaths.Count);
             var biomeIndex = biomes.IndexOf(biome);
             if (biomeIndex == -1)
                 return null;
+            if (graphics[biomeIndex] == null)
+                return graphics[biomeIndex] = GraphicDatabase.Get<Graphic_Random>(graphicPaths[biomeIndex]);
             return graphics[biomeIndex];
         }
 
         public Graphic LeaflessGraphicPerBiome(BiomeDef biome)
         {
-            if (leaflessGraphics == null)
+            if (leaflessGraphicPaths == null)
                 return null;
+            if (leaflessGraphics == null)
+                leaflessGraphics = new List<Graphic>(leaflessGraphicPaths.Count);
             var biomeIndex = biomes.IndexOf(biome);
             if (biomeIndex == -1)
                 return null;
+            if (leaflessGraphics[biomeIndex] == null)
+                return leaflessGraphics[biomeIndex] = GraphicDatabase.Get<Graphic_Random>(leaflessGraphicPaths[biomeIndex]);
             return leaflessGraphics[biomeIndex];
         }
 
         public Graphic SowingGraphicPerBiome(BiomeDef biome)
         {
-            if (sowingGraphics == null)
+            if (sowingGraphicPaths == null)
                 return null;
+            if (sowingGraphics == null)
+                sowingGraphics = new List<Graphic>(sowingGraphicPaths.Count);
             var biomeIndex = biomes.IndexOf(biome);
             if (biomeIndex == -1)
                 return null;
+            if (sowingGraphics[biomeIndex] == null)
+                return sowingGraphics[biomeIndex] = GraphicDatabase.Get<Graphic_Random>(sowingGraphicPaths[biomeIndex]);
             return sowingGraphics[biomeIndex];
         }
 
         public Graphic ImmatureGraphicPerBiome(BiomeDef biome)
         {
-            if (immatureGraphics == null)
+            if (immatureGraphicPaths == null)
                 return null;
+            if (immatureGraphics == null)
+                immatureGraphics = new List<Graphic>(immatureGraphicPaths.Count);
             var biomeIndex = biomes.IndexOf(biome);
             if (biomeIndex == -1)
                 return null;
+            if (immatureGraphics[biomeIndex] == null)
+                return immatureGraphics[biomeIndex] = GraphicDatabase.Get<Graphic_Random>(immatureGraphicPaths[biomeIndex]);
             return immatureGraphics[biomeIndex];
         }
     }
