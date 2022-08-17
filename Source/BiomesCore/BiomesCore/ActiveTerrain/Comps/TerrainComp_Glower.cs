701bdb75c5a16b09b0e03880de3200d04eb322d7
Taranchuk <nfhfyxer@gmail.com>
2022-08-17 06:41:39 +0300

    removed logging

--- Source/BiomesCore/BiomesCore/ActiveTerrain/Comps/TerrainComp_Glower.cs
+++ Source/BiomesCore/BiomesCore/ActiveTerrain/Comps/TerrainComp_Glower.cs
@@ -43,7 +43,6 @@ namespace BiomesCore
         public void UpdateLit()
         {
             bool shouldBeLitNow = ShouldBeLitNow;
-            Log.Message("Make it work goddamit: " + shouldBeLitNow);
             if (currentlyOn == shouldBeLitNow)
             {
                 return;
@@ -70,7 +69,6 @@ namespace BiomesCore
             {
                 parent.Map.glowGrid.RegisterGlower(AsThingComp);
             }
-            Log.Message("Make it work goddamit");
         }
 
         public override void PostRemove()
