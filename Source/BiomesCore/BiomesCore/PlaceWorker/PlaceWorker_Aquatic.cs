04c00fe28df6742b31df370c209da38dea02bb1e
XidonEye <77588639+XidonEye@users.noreply.github.com>
2022-09-29 22:08:52 +0200

    PlaceWorker_Aquatic

---
+++ Source/BiomesCore/BiomesCore/PlaceWorker/PlaceWorker_Aquatic.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesCore
+{
+    public class PlaceWorker_Aquatic : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(
+          BuildableDef checkingDef,
+          IntVec3 loc,
+          Rot4 rot,
+          Map map,
+          Thing thingToIgnore = null,
+          Thing thing = null)
+        {
+            foreach (IntVec3 aquaticCell in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            {
+                if (!map.terrainGrid.TerrainAt(aquaticCell).affordances.Contains(TerrainAffordanceDefOf.Bridgeable))
+                    return new AcceptanceReport((string)"TerrainCannotSupport_TerrainAffordance".Translate((NamedArgument)(Def)checkingDef, (NamedArgument)(Def)TerrainAffordanceDefOf.Bridgeable));
+            }
+            return true;
+        }
+    }
+}
