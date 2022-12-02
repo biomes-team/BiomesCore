42ea355e4c71cbea1a3ceb8c672209ec5b121abc
XidonEye <77588639+XidonEye@users.noreply.github.com>
2022-12-02 19:45:38 +0100

    CompProperties_CorpseThingSpawner
    better layout, add body size calculating

--- Source/BiomesCore/BiomesCore/ThingComponents/CompCorpseThingSpawner.cs
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompCorpseThingSpawner.cs
@@ -3,6 +3,18 @@ using Verse;
 
 namespace BiomesCore
 {
+    public class CompProperties_CorpseThingSpawner : CompProperties
+    {
+        public string filthDef = null;
+        public string thingDef = null;
+        public int filthAmount = 0;
+        public int baseResourceAmount = 1;
+        public CompProperties_CorpseThingSpawner() => this.compClass = typeof(CorpseThingSpawner);
+    }
+    public class CorpseThingSpawner : ThingComp
+    {
+        public CompProperties_CorpseThingSpawner Props => (CompProperties_CorpseThingSpawner)this.props;
+    }
     public class DeathActionWorker_CorpseThingSpawner : DeathActionWorker
     {
         public override void PawnDied(Corpse corpse)
@@ -12,22 +24,10 @@ namespace BiomesCore
             {
                 FilthMaker.TryMakeFilth(corpse.Position, corpse.Map, DefDatabase<ThingDef>.GetNamedSilentFail(comp.Props.filthDef), comp.Props.filthAmount, FilthSourceFlags.None);
                 Thing thing = ThingMaker.MakeThing(DefDatabase<ThingDef>.GetNamedSilentFail(comp.Props.thingDef), null);
-                thing.stackCount = comp.Props.resourceAmount;
+                thing.stackCount = (int)GenMath.RoundRandom(comp.Props.baseResourceAmount * corpse.InnerPawn.BodySize);
                 GenPlace.TryPlaceThing(thing, corpse.Position, corpse.Map, 0, null, null, default);
-                corpse.Destroy(0);
+                corpse.DeSpawn();
             }
         }
-    }
-    public class CompProperties_CorpseThingSpawner : CompProperties
-    {
-        public string filthDef = null;
-        public string thingDef = null;
-        public int filthAmount = 0;
-        public int resourceAmount = 1;
-        public CompProperties_CorpseThingSpawner() => this.compClass = typeof(CorpseThingSpawner);
-    }
-    public class CorpseThingSpawner : ThingComp
-    {
-        public CompProperties_CorpseThingSpawner Props => (CompProperties_CorpseThingSpawner)this.props;
     }
 }
