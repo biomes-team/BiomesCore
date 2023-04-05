f741571a18731742271d6453bbfd25537656ef9d
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-04-05 15:37:28 +0200

    CompProperties_CustomThingEater now supports an additional filthNutrition field.

--- Source/BiomesCore/BiomesCore/ThingComponents/CompCustomThingEater.cs
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompCustomThingEater.cs
@@ -10,18 +10,73 @@ namespace BiomesCore
     
     public class CompProperties_CustomThingEater : CompProperties
     {
-        public List<string> thingsToNutritionMapper;
+        /// <summary>
+        /// List of strings following the format ThingDef~NutritionValue.
+        /// </summary>
+        public List<string> thingsToNutritionMapper = new List<string>();
+
+        /// <summary>
+        /// This creature can eat all filth which can be washed away by rain. Eating filth provides this nutrition.
+        /// thingsToNutritionMapper can be used to override specific filth nutrition values.
+        /// </summary>
+        public float filthNutrition;
+
+        /// <summary>
+        /// Lazily initialized set of all filth that creatures with a filthNutrition value can eat.
+        /// </summary>
+        private static HashSet<ThingDef> _filth;
 
         [Unsaved]
         public Dictionary<ThingDef, float> thingsToNutrition = new Dictionary<ThingDef, float>();
 
         public CompProperties_CustomThingEater() => compClass = typeof(CompCustomThingEater);
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach (var line in base.ConfigErrors(parentDef))
+            {
+                yield return line;
+            }
+
+            if (thingsToNutritionMapper.Count == 0 && filthNutrition <= 0.0F)
+            {
+                yield return "CompProperties_CustomThingEater must define some custom things to eat";
+            }
+        }
+
+        private void CalculateFilthNutrition()
+        {
+            if (filthNutrition <= 0.0F)
+            {
+                return;
+            }
+
+            if (_filth == null)
+            {
+                _filth = new HashSet<ThingDef>();
+                foreach (ThingDef filthDef in DefDatabase<ThingDef>.AllDefs)
+                {
+                    if (filthDef.filth == null || !filthDef.filth.rainWashes)
+                    {
+                        continue;
+                    }
+
+                    _filth.Add(filthDef);
+                }
+            }
+
+            foreach (ThingDef filthDef in _filth)
+            {
+                thingsToNutrition[filthDef] = filthNutrition;
+            }
+        }
         
         public override void ResolveReferences(ThingDef parentDef)
         {
             base.ResolveReferences(parentDef);
 
-            thingsToNutrition.Clear();
+            CalculateFilthNutrition();
+
             foreach (var str in thingsToNutritionMapper)
             {
                 var parts = str.Split('~');
