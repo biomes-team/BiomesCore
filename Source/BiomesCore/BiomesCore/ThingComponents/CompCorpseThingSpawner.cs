2c04fd05c2efac76b2549bfffc2a33dd9d021813
XidonEye <77588639+XidonEye@users.noreply.github.com>
2023-04-16 14:12:44 +0200

    changed comp name

--- Source/BiomesCore/BiomesCore/ThingComponents/CompCorpseThingSpawner.cs
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompCorpseThingSpawner.cs
@@ -9,9 +9,9 @@ namespace BiomesCore
         public string thingDef = null;
         public int filthAmount = 0;
         public int baseResourceAmount = 1;
-        public CompProperties_CorpseThingSpawner() => this.compClass = typeof(CorpseThingSpawner);
+        public CompProperties_CorpseThingSpawner() => this.compClass = typeof(CompCorpseThingSpawner);
     }
-    public class CorpseThingSpawner : ThingComp
+    public class CompCorpseThingSpawner : ThingComp
     {
         public CompProperties_CorpseThingSpawner Props => (CompProperties_CorpseThingSpawner)this.props;
     }
@@ -19,7 +19,7 @@ namespace BiomesCore
     {
         public override void PawnDied(Corpse corpse)
         {
-            CorpseThingSpawner comp = corpse.InnerPawn.TryGetComp<CorpseThingSpawner>();
+            CompCorpseThingSpawner comp = corpse.InnerPawn.TryGetComp<CompCorpseThingSpawner>();
             if (comp != null)
             {
                 FilthMaker.TryMakeFilth(corpse.Position, corpse.Map, DefDatabase<ThingDef>.GetNamedSilentFail(comp.Props.filthDef), comp.Props.filthAmount, FilthSourceFlags.None);
