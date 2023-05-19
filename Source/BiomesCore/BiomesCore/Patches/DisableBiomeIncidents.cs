34fd449385c4738308d7dca1901dd447dcdee059
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-05-19 20:19:00 +0200

    Disable incidents code now takes into account conditions.

--- Source/BiomesCore/BiomesCore/Patches/DisableBiomeIncidents.cs
+++ Source/BiomesCore/BiomesCore/Patches/DisableBiomeIncidents.cs
@@ -7,35 +7,73 @@ using BiomesCore.DefModExtensions;
 namespace BiomesCore.Patches
 {
 	[HarmonyPatch(typeof(IncidentWorker), nameof(IncidentWorker.CanFireNow))]
-	static class DisableBiomeIncidents
+	internal static class DisableBiomeIncidents
 	{
-		private static HashSet<IncidentDef> _forbiddenIncidents;
+		private static HashSet<IncidentDef> _forbiddenCavernIncidentDefs;
 
 		private static void Initialize()
 		{
-			if (_forbiddenIncidents != null)
+			if (_forbiddenCavernIncidentDefs != null)
 			{
 				return;
 			}
 
-			_forbiddenIncidents = new HashSet<IncidentDef>();
+			_forbiddenCavernIncidentDefs = new HashSet<IncidentDef>();
 
 			foreach (var def in DefDatabase<DisableIncidentsDef>.AllDefs)
 			{
 				if (def.isCavern)
 				{
-					_forbiddenIncidents.AddRange(def.incidents);
+					_forbiddenCavernIncidentDefs.AddRange(def.incidents);
 				}
 			}
 		}
 
-		static bool Prefix(IncidentParms parms, ref IncidentWorker __instance, ref bool __result)
+		private static List<Map> MapTargets(IncidentDef def, IIncidentTarget target)
+		{
+			List<Map> maps = new List<Map>();
+			if (def.gameCondition != null && target.GameConditionManager != null)
+			{
+				var currentMaps = Find.Maps;
+				for (var index = 0; index < maps.Count; ++index)
+				{
+					var map = currentMaps[index];
+					if (map.IsPlayerHome)
+					{
+						maps.Add(map);
+					}
+				}
+			}
+			else if (target is Map map)
+			{
+				maps.Add(map);
+			}
+
+			return maps;
+		}
+
+		private static bool ShouldDisableIncident(IncidentDef def, List<Map> targetMaps)
+		{
+			for (var index = 0; index < targetMaps.Count; ++index)
+			{
+				var map = targetMaps[index];
+				var extension = map.Biome.GetModExtension<BiomesMap>();
+				if (extension != null && extension.isCavern && _forbiddenCavernIncidentDefs.Contains(def))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private static bool Prefix(IncidentParms parms, ref IncidentWorker __instance, ref bool __result)
 		{
 			Initialize();
 
-			if (parms.target is Map map && map.Biome.HasModExtension<BiomesMap>() &&
-			    map.Biome.GetModExtension<BiomesMap>().isCavern &&
-			    _forbiddenIncidents.Contains(__instance.def))
+			var incidentDef = __instance.def;
+			var targets = MapTargets(incidentDef, parms.target);
+			if (ShouldDisableIncident(incidentDef, targets))
 			{
 				__result = false;
 				return false;
