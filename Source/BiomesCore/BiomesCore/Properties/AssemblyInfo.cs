38df2b1ce1b81fcbcb90f3117c1b5cdf948af9a6
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-05-30 22:51:12 +0200

    Bump version to 2.2.0

--- Source/BiomesCore/BiomesCore/Properties/AssemblyInfo.cs
+++ Source/BiomesCore/BiomesCore/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Biomes Core")]
-[assembly: AssemblyCopyright("Copyright © The Biomes Team 2019")]
+[assembly: AssemblyCopyright("Copyright © The Biomes Team 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.1.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyInformationalVersion("2.2.0.0")]
