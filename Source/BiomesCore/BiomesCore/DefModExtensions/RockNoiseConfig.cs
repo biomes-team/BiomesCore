029f136fe340d1636c0718164fbae45ce892dd03
m00nl1ght <github@m00nl1ght.dev>
2023-06-01 00:51:35 +0200

    Add RockNoiseConfig and patch

---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/RockNoiseConfig.cs
@@ -0,0 +1,10 @@
+using Verse;
+
+namespace BiomesCore.DefModExtensions
+{
+	public class RockNoiseConfig : DefModExtension
+	{
+		public float frequency = 0.005f;
+		public float offset = 0f;
+	}
+}
---
+++ Source/BiomesCore/BiomesCore/Patches/RockNoises_Init.cs
@@ -0,0 +1,35 @@
+﻿using BiomesCore.DefModExtensions;
+using HarmonyLib;
+using Verse;
+using Verse.Noise;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(RockNoises), "Init")]
+    internal static class RockNoises_Init
+    {
+        static void Postfix()
+        {
+            foreach (var rockNoise in RockNoises.rockNoises)
+            {
+                var config = rockNoise.rockDef.GetModExtension<RockNoiseConfig>();
+                if (config != null)
+                {
+                    if (rockNoise.noise is Perlin original)
+                    {
+                        var perlin = new Perlin(
+                            config.frequency, 
+                            original.Lacunarity, 
+                            original.Persistence, 
+                            original.OctaveCount, 
+                            original.Seed, 
+                            original.Quality
+                        );
+
+                        rockNoise.noise = new Add(perlin, new Const(config.offset));
+                    }
+                }
+            }
+        }
+    }
+}
