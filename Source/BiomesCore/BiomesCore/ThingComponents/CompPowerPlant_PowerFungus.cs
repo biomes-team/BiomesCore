5d51a687dc1952f28aeb01e6f58a9ee5c8f81690
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-06-24 05:09:03 +0300

    PowerFungus Component
    Added the component needed for the mushroom generator.
    
    Counts the number of plants attached to the generator and generates energy based on their lifespan.
    
    I was thinking of making it dependent on plant growth, but judging by the way the facilities (through which I did the plant count) work, this can lead to a strange bug, with a slow recalculation.

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompPowerPlant_PowerFungus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BiomesCore
+{
+	public class CompPowerPlant_PowerFungus : CompPowerPlant
+	{
+		
+
+		public CompAffectedByFacilities Facilities => parent.TryGetComp<CompAffectedByFacilities>();
+
+		public override void UpdateDesiredPowerOutput()
+		{
+			base.PowerOutput = GetAllPowerPlants();
+		}
+
+		public int GetAllPowerPlants()
+		{
+			float powerPlantCount = 0;
+			if (Facilities != null)
+			{
+				List<Thing> linkedFacilitiesListForReading = Facilities.LinkedFacilitiesListForReading;
+				foreach (Thing item in linkedFacilitiesListForReading)
+				{
+					if (item.def.plant != null)
+					{
+						float powerPotential = item.def.plant.LifespanDays;
+						powerPlantCount += powerPotential;
+					}
+				}
+			}
+			int powerPlantCost = (int)powerPlantCount;
+			return powerPlantCost;
+		}
+
+	}
+
+}
