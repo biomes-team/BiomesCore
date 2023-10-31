ca43ba0990597c3360b9f47058b41d18172b4ff6
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2023-10-31 04:05:28 +0300

    CompInitialHediff
    Adds hediffs to the pawn from the list when spawning and loading.
    
    Currently used for Waste Hound from Polluted Lands. But I think this comp is quite universal.

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompProperties_InitialHediff.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using Verse;
+
+namespace BiomesCore
+{
+
+	// Adds hediffs from the list when a pawn spawns and after a save-load.
+	// Created for animals from Polluted Lands
+	public class CompProperties_InitialHediff : CompProperties
+	{
+
+		public List<HediffDef> hediffDefs;
+
+		public CompProperties_InitialHediff()
+		{
+			compClass = typeof(CompInitialHediff);
+		}
+	}
+
+	public class CompInitialHediff : ThingComp
+	{
+
+		private CompProperties_InitialHediff Props => (CompProperties_InitialHediff)props;
+
+		public override void PostSpawnSetup(bool respawningAfterLoad)
+		{
+			base.PostSpawnSetup(respawningAfterLoad);
+			AddHediff();
+		}
+
+		public void AddHediff()
+		{
+			Pawn pawn = parent as Pawn;
+			foreach (HediffDef hediff in Props.hediffDefs)
+			{
+				if (!pawn.health.hediffSet.HasHediff(hediff))
+				{
+					pawn.health.AddHediff(hediff);
+				}
+			}
+		}
+	}
+
+}
