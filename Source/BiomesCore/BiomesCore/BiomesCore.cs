472afdc5ebeaf10a9a9a1f33bbb5aa933996ee67
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-11-02 22:33:55 +0100

    Bump version to 2.5.0

--- Source/BiomesCore/BiomesCore/BiomesCore.cs
+++ Source/BiomesCore/BiomesCore/BiomesCore.cs
@@ -7,28 +7,28 @@ using Verse;
 
 namespace BiomesCore
 {
-    [StaticConstructorOnStartup]
-    public static class BiomesCore
-    {
-        public static Harmony HarmonyInstance;
-        public const string Id = "rimworld.biomes.core";
-        public const string Name = "Biomes! Core";
-        public static string Version = (Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute).InformationalVersion;
+	[StaticConstructorOnStartup]
+	public static class BiomesCore
+	{
+		public static Harmony HarmonyInstance;
+		public const string Id = "rimworld.biomes.core";
+		public const string Name = "Biomes! Core";
+		private static readonly Version Version = typeof(BiomesCore).Assembly.GetName().Version;
 
-        static BiomesCore()
-        {
-            HarmonyInstance = new Harmony(BiomesCore.Id);
-            HarmonyInstance.PatchAll();
-            LongEventHandler.ExecuteWhenFinished(Patches.ExtraStatInfo.Initialize);
+		static BiomesCore()
+		{
+			HarmonyInstance = new Harmony(BiomesCore.Id);
+			HarmonyInstance.PatchAll();
+			LongEventHandler.ExecuteWhenFinished(Patches.ExtraStatInfo.Initialize);
 
-            // Conditional Harmony patches. Mostly intended for mod compatibility.
-            PawnGroupKindWorker_Trader_GenerateCarriers.ModCompatibility();
-            Log("Initialized");
-        }
+			// Conditional Harmony patches. Mostly intended for mod compatibility.
+			PawnGroupKindWorker_Trader_GenerateCarriers.ModCompatibility();
+			Log("Initialized");
+		}
 
-        public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
-        public static void Error(string message) => Verse.Log.Error(PrefixMessage(message));
+		public static void Log(string message) => Verse.Log.Message(PrefixMessage(message));
+		public static void Error(string message) => Verse.Log.Error(PrefixMessage(message));
 
-        private static string PrefixMessage(string message) => $"[{Name} v{Version}] {message}";
-    }
+		private static string PrefixMessage(string message) => $"[{Name} v{Version}] {message}";
+	}
 }
