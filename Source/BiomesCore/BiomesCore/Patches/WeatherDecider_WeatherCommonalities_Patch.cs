605d474cf2badf333bb4209a2c42941a94075e95
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2023-12-05 07:45:22 +0100

    ModExtension BiomesMap.disallowedWeathers now also takes effect after map generation.

---
+++ Source/BiomesCore/BiomesCore/Patches/WeatherDecider_WeatherCommonalities_Patch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BiomesCore.DefModExtensions;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BiomesCore.Patches
+{
+	/// <summary>
+	/// See ModExtension BiomesMap.disallowedWeathers.
+	/// </summary>
+	[HarmonyPatch(typeof(WeatherDecider), nameof(WeatherDecider.WeatherCommonalities), MethodType.Getter)]
+	public static class WeatherDecider_WeatherCommonalities_Patch
+	{
+		public static IEnumerable<WeatherCommonalityRecord> Postfix(IEnumerable<WeatherCommonalityRecord> values,
+			Map ___map)
+		{
+			HashSet<WeatherDef> disallowedWeathers =
+				___map.Biome.GetModExtension<BiomesMap>()?.disallowedWeathers?.ToHashSet();
+
+			foreach (WeatherCommonalityRecord record in values)
+			{
+				if (disallowedWeathers == null || !disallowedWeathers.Contains(record.weather))
+				{
+					yield return record;
+				}
+			}
+		}
+	}
+}
