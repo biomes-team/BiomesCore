7c03fd53c1ce9b461c92638a444ed52b89ec4b0b
José Ángel Soler Ortiz <jose.angel.soler.ortiz@gmail.com>
2024-03-08 19:37:21 +0100

    Minor improvements to JobDriver_EatCustomThing.

--- Source/BiomesCore/BiomesCore/Jobs/JobDriver_EatCustomThing.cs
+++ Source/BiomesCore/BiomesCore/Jobs/JobDriver_EatCustomThing.cs
@@ -1,45 +1,46 @@
-using RimWorld;
 using System.Collections.Generic;
+using RimWorld;
+using Verse;
 using Verse.AI;
 
 namespace BiomesCore
 {
-    public class JobDriver_EatCustomThing : JobDriver
-    {
-        public override bool TryMakePreToilReservations(bool errorOnFailed) =>
-            pawn.Reserve(TargetA, job, errorOnFailed: errorOnFailed);
-        
-        protected override IEnumerable<Toil> MakeNewToils()
-        {
-            yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
-            
-            var wait = new Toil
-            {
-                defaultCompleteMode = ToilCompleteMode.Delay,
-                defaultDuration = 500,
-                socialMode = RandomSocialMode.Off
-            };
-            
-            wait.FailOnCannotTouch(TargetIndex.A, PathEndMode.Touch);
-            
-            yield return wait.WithProgressBarToilDelay(TargetIndex.A, true);
-            
-            yield return Toils_General.Do(() =>
-            {
-                var customThingEater = pawn.GetComp<CompCustomThingEater>();
-                if (customThingEater != null)
-                {
-                    var thing = TargetA.Thing;
-                    if (thing == null) return;
+	public class JobDriver_EatCustomThing : JobDriver
+	{
+		public override bool TryMakePreToilReservations(bool errorOnFailed) =>
+			pawn.Reserve(TargetA, job, errorOnFailed: errorOnFailed);
 
-                    if (customThingEater.Props.thingsToNutrition.TryGetValue(thing.def, out var nutrition))
-                    {
-                        pawn.needs.food.CurLevel += nutrition;
-                        if (thing.stackCount > 1) thing.SplitOff(1);
-                        else thing.Destroy();
-                    }
-                }
-            });
-        }
-    }
+		protected override IEnumerable<Toil> MakeNewToils()
+		{
+			yield return Toils_Goto.GotoCell(TargetIndex.A, PathEndMode.OnCell);
+
+			var wait = new Toil
+			{
+				defaultCompleteMode = ToilCompleteMode.Delay,
+				defaultDuration = 500,
+				socialMode = RandomSocialMode.Off
+			};
+
+			wait.FailOnCannotTouch(TargetIndex.A, PathEndMode.Touch);
+
+			yield return wait.WithProgressBarToilDelay(TargetIndex.A, true);
+
+			CompCustomThingEater customThingEater = pawn.GetComp<CompCustomThingEater>();
+			if (customThingEater == null)
+			{
+				yield break;
+			}
+
+			Dictionary<ThingDef, float> thingsToNutrition = customThingEater.Props.thingsToNutrition;
+			yield return Toils_General.Do(() =>
+			{
+				if (TargetA.Thing is Filth filth && thingsToNutrition.TryGetValue(filth.def, out var nutrition))
+				{
+					pawn.needs.food.CurLevel += nutrition;
+					filth.Destroy();
+				}
+				ReadyForNextToil();
+			});
+		}
+	}
 }
