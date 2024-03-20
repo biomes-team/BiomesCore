6ab7606e565629a857e89479711a7cc2abcc29b8
Thekiborg <none>
2024-03-20 19:17:41 +0100

    Added missing files

---
+++ Source/BiomesCore/BiomesCore/DeathActionWorkers/DeathActionWorker_ReleaseGasCloud.cs
@@ -0,0 +1,24 @@
+﻿using Verse;
+using Verse.AI.Group;
+
+namespace BiomesCore
+{
+    public class DeathActionWorker_ReleaseGasCloud : DeathActionWorker
+    {
+        public override bool DangerousInMelee => true;
+        public override void PawnDied(Corpse corpse, Lord prevLord)
+        {
+            Pawn deceasedPawn = corpse.InnerPawn;
+            ModExtension_ReleaseGasCloud modExtension = deceasedPawn.def.GetModExtension<ModExtension_ReleaseGasCloud>();
+            if (modExtension == null)
+            {
+                Log.Error($"ModExtension_ReleaseGasCloud is needed for DeathActionWorker_ReleaseGasCloud to work on pawn {deceasedPawn.def.defName}");
+                return;
+            }
+            if (!corpse.DestroyedOrNull())
+            {
+                GasUtility.AddGas(corpse.Position, corpse.Map, modExtension.gasType, modExtension.amountOfGasFloat);
+            }
+        }
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/ModExtension_ReleaseGasCloud.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace BiomesCore
+{
+    public class ModExtension_ReleaseGasCloud : DefModExtension
+    {
+        public GasType gasType = GasType.ToxGas; // BlindSmoke, ToxGas, RotStink, Unused.
+        public float amountOfGasFloat = 1f;
+    }
+}
