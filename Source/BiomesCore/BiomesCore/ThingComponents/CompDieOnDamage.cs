87c55594d1bd2c80ef1758cb63951d3d68eeb1f5
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-07-23 20:53:02 +0200

    added CompDieOnDamage

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompDieOnDamage.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompDieOnDamage : ThingComp
+    {
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.PostPostApplyDamage(dinfo, totalDamageDealt);
+            if (Rand.Chance(Props.procChance))
+            {
+                parent.Kill();
+            }
+        }
+
+        public CompProperties_DieOnDamage Props => props as CompProperties_DieOnDamage;
+    }
+
+    public class CompProperties_DieOnDamage : CompProperties
+    {
+        public float procChance = 1f;
+        
+        public CompProperties_DieOnDamage()
+        {
+            compClass = typeof(CompDieOnDamage);
+        }
+    }
+}
