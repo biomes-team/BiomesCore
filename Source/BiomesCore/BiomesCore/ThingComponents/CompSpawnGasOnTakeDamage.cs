920e523f60b35d3249f513c5b7acae2aed83d264
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-08-11 17:54:19 +0200

    added CompSpawnGasOnTakeDamage

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompSpawnGasOnTakeDamage.cs
@@ -0,0 +1,31 @@
+﻿using Verse;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompSpawnGasOnTakeDamage : ThingComp
+    {
+        public CompProperties_SpawnGasOnTakeDamage Props => (CompProperties_SpawnGasOnTakeDamage)props;
+
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            if (parent.Spawned && Rand.Chance(Props.chance))
+            {
+                GasUtility.AddGas(parent.Position, parent.Map, Props.gasType,
+                    Props.amount);
+            }
+        }
+    }
+
+    public class CompProperties_SpawnGasOnTakeDamage : CompProperties
+    {
+        public CompProperties_SpawnGasOnTakeDamage()
+        {
+            compClass = typeof(CompSpawnGasOnTakeDamage);
+        }
+
+        public GasType gasType = GasType.ToxGas;
+
+        public float chance = 1f;
+        public int amount = 255;
+    }
+}
