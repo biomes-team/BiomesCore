e34591f3d946a8b8ccf3d4fb101c5f32e9eac483
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-09-29 18:09:48 +0200

    implemented CompetitionHunterModExtension

---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/CompetitionHunterModExtension.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace BiomesCore.DefModExtensions
+{
+    public class CompetitionHunterModExtension : DefModExtension
+    {
+        public bool maleEnabled = false;
+        public bool femaleEnabled = false;
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/Patches/CompetitionHunterPatches.cs
@@ -0,0 +1,47 @@
+﻿using BiomesCore.DefModExtensions;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(FoodUtility))]
+    internal static class CompetitionHunterPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("IsAcceptablePreyFor")]
+        public static void CompetitionHunterPatch_IsAcceptablePreyFor(Pawn predator,
+            Pawn prey,
+            ref bool __result)
+        {
+            var competitionHunter =
+                predator.def.GetModExtension<CompetitionHunterModExtension>();
+            if (competitionHunter == null) return;
+            if (predator.def != prey.def || predator.gender != prey.gender) return;
+
+            if ((predator.gender == Gender.Female &&
+                 competitionHunter.femaleEnabled) ||
+                (predator.gender == Gender.Male && competitionHunter.maleEnabled))
+            {
+                __result = true;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("GetPreyScoreFor")]
+        public static void CompetitionHunterPatch_PreyScoreFor(Pawn predator, Pawn prey,
+            ref float __result)
+        {
+            var competitionHunter =
+                predator.def.GetModExtension<CompetitionHunterModExtension>();
+            if (competitionHunter == null) return;
+            if (predator.def != prey.def || predator.gender != prey.gender) return;
+
+            if ((predator.gender == Gender.Female && competitionHunter.femaleEnabled) ||
+                (predator.gender == Gender.Male && competitionHunter.maleEnabled))
+            {
+                __result *= 1000f;
+            }
+        }
+    }
+}
