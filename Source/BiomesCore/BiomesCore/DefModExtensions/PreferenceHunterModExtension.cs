356409bd94f948e87dec0dd169f39438310bd956
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-10-22 21:34:24 +0200

    added preference hunter mod extension

---
+++ Source/BiomesCore/BiomesCore/DefModExtensions/PreferenceHunterModExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace BiomesCore.DefModExtensions
+{
+    public class PreferenceHunterModExtension : DefModExtension
+    {
+        public List<ThingDef> huntingPrefs;
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/Patches/PreferenceHunterPatches.cs
@@ -0,0 +1,44 @@
+﻿using BiomesCore.DefModExtensions;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace BiomesCore.Patches
+{
+    [HarmonyPatch(typeof(FoodUtility))]
+    internal static class PreferenceHunterPatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("IsAcceptablePreyFor")]
+        public static void CompetitionHunterPatch_IsAcceptablePreyFor(Pawn predator,
+            Pawn prey,
+            ref bool __result)
+        {
+            var competitionHunter =
+                predator.def.GetModExtension<PreferenceHunterModExtension>();
+            if (competitionHunter == null) return;
+            if (predator.def != prey.def || predator.gender != prey.gender) return;
+
+            if (competitionHunter.huntingPrefs.Contains(prey.def))
+            {
+                __result = true;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("GetPreyScoreFor")]
+        public static void CompetitionHunterPatch_PreyScoreFor(Pawn predator, Pawn prey,
+            ref float __result)
+        {
+            var competitionHunter =
+                predator.def.GetModExtension<PreferenceHunterModExtension>();
+            if (competitionHunter == null) return;
+            if (predator.def != prey.def || predator.gender != prey.gender) return;
+
+            if (competitionHunter.huntingPrefs.Contains(prey.def))
+            {
+                __result *= 1000f;
+            }
+        }
+    }
+}
