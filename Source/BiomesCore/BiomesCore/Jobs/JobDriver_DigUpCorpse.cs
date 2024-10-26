d1650bd111ee586e9e800b142f84f0e2043cb362
Alite <128163141+KonradHeinser@users.noreply.github.com>
2024-10-26 12:18:48 -0400

    Dig up corpse source

---
+++ Source/BiomesCore/BiomesCore/Jobs/JobDriver_DigUpCorpse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+using RimWorld;
+using UnityEngine;
+
+namespace BiomesCore
+{
+    public class JobDriver_DigUpCorpse : JobDriver
+    {
+        public override string GetReport()
+        {
+            return job.def.reportString.Formatted(TargetA.Label);
+        }
+
+        public override bool TryMakePreToilReservations(bool errorOnFailed) => true; // No reservations needed, and no fail point
+
+        protected override IEnumerable<Toil> MakeNewToils()
+        {
+            Toil toil = ToilMaker.MakeToil("DigUpCorpse");
+
+            toil.initAction = delegate
+            {
+                toil.actor.pather.StopDead();
+                toil.actor.jobs.curDriver.ticksLeftThisToil = job.count; // Count represents time because there wasn't any other variable I could access from the giver's side
+            };
+            toil.WithProgressBar(TargetIndex.B, delegate
+            {
+                return (float)toil.actor.jobs.curDriver.ticksLeftThisToil / Mathf.Round((float)job.count);
+            });
+            toil.AddFinishAction(delegate
+            {
+                GenSpawn.Spawn(TargetA.Pawn, toil.actor.Position, toil.actor.Map);
+                TargetA.Pawn.Kill(new DamageInfo(DamageDefOf.Bite, 99999f, 999f, -1f));
+                if (toil.actor.Faction == Faction.OfPlayer)
+                    TargetA.Pawn?.Corpse?.SetForbidden(false);
+            });
+            yield return toil;
+        }
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/Jobs/JobGiver_DigUpCorpse.cs
@@ -0,0 +1,55 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace BiomesCore
+{
+    public class JobGiver_DigUpCorpse : ThinkNode_JobGiver
+    {
+        public List<PawnKindDef> diggerKinds; // Mostly used to limit this job to be used by certain animals
+
+        public List<PawnKindDef> corpseKinds; // Picks a random one of these to dig up
+
+        public JobDef jobDef = null; // Job to give pawn. 
+
+        public int jobTicks = 625;
+
+        public int hashInterval = 60; // Makes it more chance based
+
+        public bool onlyNaturalTerrain = true;
+
+        public float minFertility = 0f; // The minimum fertility the ground should have to dig something up
+
+        public override float GetPriority(Pawn pawn)
+        {
+            if (!diggerKinds.NullOrEmpty() && !diggerKinds.Contains(pawn.kindDef))
+                return 0f;
+            return 9.5f;
+        }
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            if (!diggerKinds.NullOrEmpty() && !diggerKinds.Contains(pawn.kindDef))
+                return null;
+            if (!pawn.IsHashIntervalTick(hashInterval))
+                return null;
+            if (onlyNaturalTerrain && !pawn.Position.GetTerrain(pawn.Map).natural)
+                return null;
+            if (pawn.Position.GetTerrain(pawn.Map).fertility < minFertility)
+                return null;
+
+            if (!corpseKinds.NullOrEmpty())
+            {
+                Pawn corpse = PawnGenerator.GeneratePawn(corpseKinds.RandomElement());
+                if (corpse != null)
+                {
+                    Job job = JobMaker.MakeJob(jobDef, corpse, pawn);
+                    job.count = jobTicks; // Uses count because I couldn't find a time related thing that I could use.
+                    return job;
+                }
+            }
+            return null;
+        }
+    }
+}
