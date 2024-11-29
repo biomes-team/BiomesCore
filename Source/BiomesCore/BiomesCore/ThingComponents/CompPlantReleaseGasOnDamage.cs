9f164341a9de7ccaccd7b0c85a24d4151f37990b
Linniel Driver-Williams <LinnielDW@googlemail.com>
2024-11-29 22:02:38 +0100

    added CompProperties_PlantReleaseGasOnDamage and CompProperties_PlantReleaseGasOnDeath

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompPlantReleaseGasOnDamage.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompProperties_PlantReleaseGasOnDamage : CompProperties
+    {
+        public GasType gasType;
+        public float cellsToFill;
+        public EffecterDef effecterReleasing;
+        public float growthProgress = 1f;
+
+        public CompProperties_PlantReleaseGasOnDamage() =>
+            compClass = typeof(CompPlantReleaseGasOnDamage);
+    }
+
+
+    public class CompPlantReleaseGasOnDamage : ThingComp
+    {
+        public CompProperties_PlantReleaseGasOnDamage Props =>
+            (CompProperties_PlantReleaseGasOnDamage)props;
+
+        private Effecter effecter;
+
+        public override void PostPostApplyDamage(DamageInfo dinfo, float totalDamageDealt)
+        {
+            base.PostPostApplyDamage(dinfo, totalDamageDealt);
+
+            if (parent.Map == null || !(parent is Plant plant))
+                return;
+            if (!(plant.Growth >= Props.growthProgress) || plant.Dying) return;
+
+            var radius = Mathf.Round(plant.Growth * Props.cellsToFill);
+            GasUtility.AddGas(parent.PositionHeld, parent.MapHeld, Props.gasType, radius);
+            if (Props.effecterReleasing != null)
+            {
+                effecter ??= Props.effecterReleasing.Spawn((Plant)parent, parent.MapHeld);
+                effecter.EffectTick((TargetInfo)(Plant)parent, (TargetInfo)(Plant)parent);
+            }
+        }
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/ThingComponents/CompPlantReleaseGasOnDeath.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace BiomesCore.ThingComponents
+{
+    public class CompProperties_PlantReleaseGasOnDeath : CompProperties
+    {
+        public GasType gasType;
+        public float cellsToFill;
+        public EffecterDef effecterReleasing;
+        public float growthProgress = 1f;
+
+        public CompProperties_PlantReleaseGasOnDeath() =>
+            compClass = typeof(CompPlantReleaseGasOnDeath);
+    }
+
+    public class CompPlantReleaseGasOnDeath : ThingComp
+    {
+        public CompProperties_PlantReleaseGasOnDeath Props =>
+            (CompProperties_PlantReleaseGasOnDeath)props;
+
+        private Effecter effecter;
+
+        public override void Notify_Killed(Map prevMap, DamageInfo? dinfo = null)
+        {
+            base.Notify_Killed(prevMap, dinfo);
+
+            if (parent.Map == null || !(parent is Plant plant))
+                return;
+            if (!(plant.Growth >= Props.growthProgress)) return;
+
+            var radius = Mathf.Round(plant.Growth * Props.cellsToFill);
+            GasUtility.AddGas(parent.PositionHeld, parent.MapHeld, Props.gasType, radius);
+            if (Props.effecterReleasing != null)
+            {
+                effecter ??= Props.effecterReleasing.Spawn((Plant)parent, parent.MapHeld);
+                effecter.EffectTick((TargetInfo)(Plant)parent, (TargetInfo)(Plant)parent);
+            }
+        }
+    }
+}
