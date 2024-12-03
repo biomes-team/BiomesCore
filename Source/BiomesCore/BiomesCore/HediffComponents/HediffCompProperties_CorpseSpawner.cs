03e8f1af5c36c0214ff9cfff3a583c9cb95d858a
nimragiscoming <nine.seven.ninete@gmail.com>
2024-12-03 02:37:04 +0000

    added hediffcomp corpsespawner

---
+++ Source/BiomesCore/BiomesCore/HediffComponents/HediffCompProperties_CorpseSpawner.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BiomesCore
+{
+    public class HediffCompProperties_CorpseSpawner : HediffCompProperties
+    {
+        public ThingDef bloodDef;
+
+        public IntRange bloodCountRange = new IntRange(3, 6);
+
+        public int bloodRadius = 2;
+
+        public IntRange pawnCountRange = new IntRange(1, 1);
+
+        public PawnKindDef pawn;
+
+        public bool forceParentFaction = false;
+
+        public HediffCompProperties_CorpseSpawner()
+        {
+            compClass = typeof(HediffComp_CorpseSpawner);
+        }
+
+        public override void ResolveReferences(HediffDef parent)
+        {
+            if (bloodDef == null)
+            {
+                bloodDef = ThingDefOf.Filth_Blood;
+            }
+        }
+    }
+}
---
+++ Source/BiomesCore/BiomesCore/HediffComponents/HediffComp_CorpseSpawner.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BiomesCore
+{
+    public class HediffComp_CorpseSpawner : HediffComp
+    {
+        public HediffCompProperties_CorpseSpawner Props => (HediffCompProperties_CorpseSpawner)props;
+        public override void Notify_PawnKilled()
+        {
+            int splatterCount = Props.bloodCountRange.RandomInRange;
+
+            for (int i = 0; i < splatterCount; i++)
+            {
+                FilthMaker.TryMakeFilth(CellFinder.RandomClosewalkCellNear(Pawn.Position, Pawn.Map, Props.bloodRadius), Pawn.Map, Props.bloodDef, Pawn.LabelIndefinite());
+            }
+
+            Faction faction = Props.forceParentFaction == true ? Pawn.Faction : null;
+
+            PawnGenerationRequest request = new PawnGenerationRequest(Props.pawn, faction, PawnGenerationContext.NonPlayer, -1, forceGenerateNewPawn: true, allowDead: false, allowDowned: true, canGeneratePawnRelations: false, developmentalStages: DevelopmentalStage.Newborn);
+            request.DontGivePreArrivalPathway = true;
+
+            int pawnCount = Props.pawnCountRange.RandomInRange;
+            for (int i = 0; i < pawnCount; i++)
+            {
+                Pawn newPawn = PawnGenerator.GeneratePawn(request);
+
+                PawnUtility.TrySpawnHatchedOrBornPawn(newPawn, Pawn);
+            }
+        }
+    }
+}
