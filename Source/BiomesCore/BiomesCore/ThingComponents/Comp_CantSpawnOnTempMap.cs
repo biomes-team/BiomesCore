f4e0a26edc96c8252e9d0606849cb910def83a6e
nimrag-b <124409677+nimrag-b@users.noreply.github.com>
2024-12-23 17:03:33 +0000

    added cant spawn on temp map comp

---
+++ Source/BiomesCore/BiomesCore/ThingComponents/Comp_CantSpawnOnTempMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BiomesCore
+{
+    public class CompProperties_CantSpawnOnTempMap : CompProperties
+    {
+        public CompProperties_CantSpawnOnTempMap() => this.compClass = typeof(Comp_CantSpawnOnTempMap);
+    }
+
+    public class Comp_CantSpawnOnTempMap: ThingComp
+    {
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            if (parent.Map.IsTempIncidentMap)
+            {
+                this.parent.Destroy(DestroyMode.Vanish);
+            }
+        }
+    }
+}
