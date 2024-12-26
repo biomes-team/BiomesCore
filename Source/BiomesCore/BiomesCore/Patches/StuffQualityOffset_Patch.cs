31ce3c5f2226a0a835bedfc8ca703981ad989b27
WVCSergkart <114881826+WVCSergkart@users.noreply.github.com>
2024-12-26 08:20:10 +0300

    Temp statOffset fix
    Temporarily replaced with a simplified version due to a conflict with an unknown mod.

--- Source/BiomesCore/BiomesCore/Patches/StuffQualityOffset_Patch.cs
+++ Source/BiomesCore/BiomesCore/Patches/StuffQualityOffset_Patch.cs
@@ -8,57 +8,96 @@ using Verse;
 
 namespace BiomesCore.Patches
 {
-    [HarmonyPatch]
-    internal static class StuffQualityOffset_Patch
+
+    /// <summary>
+    /// Temporarily replaced with a simplified version due to a conflict with an unknown mod.
+    /// </summary>
+
+    //[HarmonyPatch]
+    //internal static class StuffQualityOffset_Patch
+    //{
+    //    [HarmonyTranspiler]
+    //    [HarmonyPatch(typeof(GenRecipe), "PostProcessProduct")]
+    //    private static IEnumerable<CodeInstruction> PostProcessProduct(IEnumerable<CodeInstruction> instructions) => Inject(instructions);
+
+    //    [HarmonyTranspiler]
+    //    [HarmonyPatch(typeof(Frame), "CompleteConstruction")]
+    //    private static IEnumerable<CodeInstruction> CompleteConstruction(IEnumerable<CodeInstruction> instructions) => Inject(instructions);
+
+    //    private static IEnumerable<CodeInstruction> Inject(IEnumerable<CodeInstruction> instructions)
+    //    {
+    //        var method = AccessTools.Method(typeof(QualityUtility), "GenerateQualityCreatedByPawn", new[] { typeof(Pawn), typeof(SkillDef) });
+    //        var infix = AccessTools.Method(typeof(StuffQualityOffset_Patch), nameof(AdjustQualityIfNeeded));
+
+    //        int count = 0;
+
+    //        foreach (var ci in instructions)
+    //        {
+    //            yield return ci;
+
+    //            if (ci.Calls(method))
+    //            {
+    //                yield return new CodeInstruction(OpCodes.Ldarg_0);
+    //                yield return new CodeInstruction(OpCodes.Call, infix);
+
+    //                count++;
+    //            }
+    //        }
+
+    //        if (count == 0)
+    //            throw new Exception("StuffQualityOffset_Patch failed");
+    //    }
+
+    //    private static QualityCategory AdjustQualityIfNeeded(QualityCategory quality, Thing thing)
+    //    {
+    //        if (thing.Stuff?.GetModExtension<StuffQualityOffset>() is {} offset)
+    //        {
+    //            int qualityNum = (int) quality + offset.qualityOffset;
+
+    //            var maxQuality = (int) offset.maxQuality;
+    //            var minQuality = (int) offset.minQuality;
+
+    //            if (qualityNum > maxQuality) qualityNum = maxQuality;
+    //            if (qualityNum < minQuality) qualityNum = minQuality;
+
+    //            quality = (QualityCategory) qualityNum;
+    //        }
+
+    //        return quality;
+    //    }
+    //}
+
+    [HarmonyPatch(typeof(CompQuality), "SetQuality")]
+    public static class CompQuality_SetQuality_Patch
     {
-        [HarmonyTranspiler]
-        [HarmonyPatch(typeof(GenRecipe), "PostProcessProduct")]
-        private static IEnumerable<CodeInstruction> PostProcessProduct(IEnumerable<CodeInstruction> instructions) => Inject(instructions);
-
-        [HarmonyTranspiler]
-        [HarmonyPatch(typeof(Frame), "CompleteConstruction")]
-        private static IEnumerable<CodeInstruction> CompleteConstruction(IEnumerable<CodeInstruction> instructions) => Inject(instructions);
-
-        private static IEnumerable<CodeInstruction> Inject(IEnumerable<CodeInstruction> instructions)
+        public static void Postfix(CompQuality __instance, QualityCategory q, ArtGenerationContext? source, ref QualityCategory ___qualityInt)
         {
-            var method = AccessTools.Method(typeof(QualityUtility), "GenerateQualityCreatedByPawn", new[] { typeof(Pawn), typeof(SkillDef) });
-            var infix = AccessTools.Method(typeof(StuffQualityOffset_Patch), nameof(AdjustQualityIfNeeded));
-
-            int count = 0;
-
-            foreach (var ci in instructions)
+            if (source == ArtGenerationContext.Colony)
             {
-                yield return ci;
-
-                if (ci.Calls(method))
-                {
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, infix);
-
-                    count++;
-                }
+                ___qualityInt = AdjustQualityIfNeeded(q, __instance.parent);
+                __instance.parent.PostQualitySet();
+                __instance.parent.TryGetComp<CompFacilityQualityBased>()?.PostQualitySet();
             }
-
-            if (count == 0)
-                throw new Exception("StuffQualityOffset_Patch failed");
         }
 
         private static QualityCategory AdjustQualityIfNeeded(QualityCategory quality, Thing thing)
         {
-            if (thing.Stuff?.GetModExtension<StuffQualityOffset>() is {} offset)
+            if (thing.Stuff?.GetModExtension<StuffQualityOffset>() is { } offset)
             {
-                int qualityNum = (int) quality + offset.qualityOffset;
+                int qualityNum = (int)quality + offset.qualityOffset;
 
-                var maxQuality = (int) offset.maxQuality;
-                var minQuality = (int) offset.minQuality;
+                var maxQuality = (int)offset.maxQuality;
+                var minQuality = (int)offset.minQuality;
 
                 if (qualityNum > maxQuality) qualityNum = maxQuality;
                 if (qualityNum < minQuality) qualityNum = minQuality;
 
-                quality = (QualityCategory) qualityNum;
+                quality = (QualityCategory)qualityNum;
             }
 
             return quality;
         }
+
     }
+
 }
